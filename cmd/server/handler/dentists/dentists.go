1ddd98ccd44eb628af0a948cf71519fc6df31a6a
Jennifer Colman <jenniferecolmanb@hotmail.com>
2023-11-28 22:32:28 -0300

    handler dentist

--- cmd/server/handler/dentists/dentists.go
+++ cmd/server/handler/dentists/dentists.go
@@ -1,1 +1,126 @@
 package dentists
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/Gigi-U/eb3_desafio_Final_grupo03.git/internal/dentists"
+	"github.com/Gigi-U/eb3_desafio_Final_grupo03.git/internal/models"
+	"github.com/gin-gonic/gin"
+)
+
+type Controller struct {
+	service dentists.Service
+}
+
+func NewControllerDentists(service dentists.Service) *Controller {
+	return &Controller{service: service}
+}
+
+// Method HandlerCreate is the handler needed to POST a dentist
+func (c *Controller) HandlerCreate() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		var dentistRequest models.Dentist
+
+		err := ctx.Bind(&dentistRequest)
+
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"message": "bad request",
+				"error":   err,
+			})
+			return
+		}
+
+		dentist, err := c.service.Create(ctx, dentistRequest)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"message": "Internal server error",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"data":    dentist,
+			"message": "Dentist created",
+		})
+
+	}
+}
+
+// Method HandlerGetByID is the handler needed to GET a dentist by its Id
+func (c *Controller) HandlerGetByID() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		// Retrieve the request Id
+		idParam := ctx.Param("id")
+
+		// Convert the string parameter to a int
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"message": "bad request",
+				"error":   err,
+			})
+			return
+		}
+
+		// Call the service to get by id
+		dentist, err := c.service.GetByID(ctx, id)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"message": "Internal server error",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"data":    dentist,
+			"message": "Dentist found",
+		})
+	}
+}
+
+// Method HandlerUpdate is the handler needed to UPDATE a dentist by its Id
+func (c *Controller) HandlerUpdate() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		// Retrieve the request Id
+		idParam := ctx.Param("id")
+
+		var dentistRequest models.Dentist
+
+		// Convert the string parameter to a int
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"message": "bad request",
+				"error":   err,
+			})
+			return
+		}
+
+		if err := ctx.Bind(&dentistRequest); err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"message": "Bad request",
+				"error":   err.Error(),
+			})
+			return
+		}
+
+		// Call the service to update
+		updatedDentist, err := c.service.Update(ctx, dentistRequest, id)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"message": "Internal server error",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"data":    updatedDentist,
+			"message": "Dentist updated",
+		})
+	}
+}
